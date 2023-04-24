d3c9a51c1aac73086d525915016ed7083a27bba6
Manoj Ranga <manojranga7583@gmail.com>
2023-04-24 17:21:10 +0530

    initial commit

---
+++ src/main/java/com/seekerscloud/nebulaservice/NebulaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.seekerscloud.nebulaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seekerscloud/nebulaservice/NebulaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seekerscloud.nebulaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
